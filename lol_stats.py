eab526891282074fe1db07aa7fdb98cf093c1c69
Yung Choi <neochoi@google.com>
2016-04-12 08:20:10 +0000

    Fix bug.

--- lol_stats.py
+++ lol_stats.py
@@ -730,11 +730,8 @@ class BuildResultPages(webapp2.RequestHandler):
             'the views or opinions of Riot Games or anyone officially involved '
             'in producing or managing League of Legends. League of Legends and '
             'Riot Games are trademarks or registered trademarks of Riot Games, '
-            'Inc. League of Legends © Riot Games, Inc.<br/>'
-           )
+            'Inc. League of Legends © Riot Games, Inc.<br/>')
   
-   'Lol stats isn\'t endorsed by Riot Games and doesn\'t reflect the views or opinions of Riot Games or anyone officially involved in producing or managing League of Legends. League of Legends and Riot Games are trademarks or registered trademarks of Riot Games, Inc. League of Legends © Riot Games, Inc.'
-
   def post(self):
     # Called by taskqueue.
     self.get()
