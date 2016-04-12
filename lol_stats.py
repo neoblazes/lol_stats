2a40787cc4c3df0bd73e22e2d00f98f7c51fa600
Yung Choi <neochoi@google.com>
2016-04-12 08:14:48 +0000

    Updates provenance.

--- lol_stats.py
+++ lol_stats.py
@@ -724,10 +724,16 @@ class BuildResultPages(webapp2.RequestHandler):
             (img, lane, name, name))
 
   def GetTimestamp(self):
-    return ('<br/>Updated: %s<br/>Under the agreement of '
-            '<a href="https://developer.riotgames.com/terms#statement">'
-            'API TERMS & CONDITIONS</a><br/>' %
-            datetime.datetime.now().strftime('%Y-%m-%d %H:%M'))
+    return ('<br/>Updated: %s<br/><br/>' %
+            datetime.datetime.now().strftime('%Y-%m-%d %H:%M') +
+            'Lol stats isn\'t endorsed by Riot Games and doesn\'t reflect '
+            'the views or opinions of Riot Games or anyone officially involved '
+            'in producing or managing League of Legends. League of Legends and '
+            'Riot Games are trademarks or registered trademarks of Riot Games, '
+            'Inc. League of Legends © Riot Games, Inc.<br/>'
+           )
+  
+   'Lol stats isn\'t endorsed by Riot Games and doesn\'t reflect the views or opinions of Riot Games or anyone officially involved in producing or managing League of Legends. League of Legends and Riot Games are trademarks or registered trademarks of Riot Games, Inc. League of Legends © Riot Games, Inc.'
 
   def post(self):
     # Called by taskqueue.
